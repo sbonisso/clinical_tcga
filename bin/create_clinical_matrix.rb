1840daaefe153e536c69c4588c73d464237670b2
Stefano <sbonisso@ucsd.edu>
2014-09-16 23:39:11 -0700

    modified bin/create_clinical_matrix.rb to use optparse

--- bin/create_clinical_matrix.rb
+++ bin/create_clinical_matrix.rb
@@ -9,19 +9,54 @@
 require 'clinical_tcga'
 require 'csv'
 require 'progressbar'
+require 'optparse'
 
-if ARGV.size != 4 then
-  puts "USAGE #{__FILE__} sample_list.txt feature_list.txt path/to/clinical/data/ output_file.csv"
+options = {}
+optparse = OptionParser.new do |opt|
+  opt.banner = "Usage: [OPTIONS]"
+  opt.separator  ""
+  opt.separator  "Options"
+
+  opt.on("-s", "--samples FILE", "file containing sample IDs") do |sampleF|
+      options[:samples] = sampleF
+  end
+
+  opt.on("-f", "--features FILE", "file containing clinical features to extract") do |featureF|
+      options[:features] = featureF
+  end
+
+  opt.on("-d", "--database FILE", "path to database ") do |databasePath|
+      options[:database] = databasePath
+  end
+  
+  opt.on("-o","--output FILE","output the matrix of samples and features") do |outFile|
+    options[:output] = outFile
+  end
+  
+  opt.on("-h","--help","help") do
+    puts optparse
+  end  
+end
+optparse.parse!
+#
+# check have minimal arguments
+if !options.has_key?(:samples) || 
+    !options.has_key?(:features) || 
+    !options.has_key?(:database) ||
+    !options.has_key?(:output) then
+  puts optparse   # print help
   Process.exit(0)
 end
 
-sample_file, feature_file, path_to_data, output_file = ARGV
+sample_file = options[:samples]
+feature_file = options[:features]
+path_to_data = options[:database]
+output_file = options[:output]
+path_to_data += "/" if (path_to_data[-1] != "/")
 
 sample_ids = IO.readlines(sample_file).map{|l| l.chomp}
 feature_v = IO.readlines(feature_file).map{|l| l.chomp}
 
-puts sample_ids.to_s
-puts feature_v.to_s
 #
 # init with samples, and desired features
 rs = ClinicalTCGA::RetrieveSamples.new(sample_ids, 
